f2229451042cf98a1229c5b57a8e82e51feb5099
AndrewKlapproth <ajk232@cornell.edu>
2018-02-28 17:31:15 -0500

    Problem Set 3

---
+++ PS3_3200.m
@@ -0,0 +1,73 @@
+%% Question 1
+% Define interval [xl xu]
+xl = 0.4; xu = 0.48;
+% Initialize midpoint
+f_xr = 1;
+% Initialize counter
+i = 1;
+
+% While the function is above tolerance
+while abs(f_xr) >= 1e-5
+    % Store old f_xr
+    f_xr_old = f_xr;
+    % Find midpoint of interval
+    xr = (xl + xu)/2;
+    % Evaluate function at xr, xl, and xu
+    f_xr = tan(pi*xr) - xr - 6;
+    f_xl = tan(pi*xl) - xl - 6;
+    f_xu = tan(pi*xu) - xu - 6;
+    % Relative error from previous step
+    error = f_xr_old - f_xr;
+    % Store values for post-processing
+    tab(i,:) = [i xl xu xr f_xr error];
+    % Find where the function changes sign and assign new interval
+    if f_xu > 0 && f_xr < 0
+        xl = xr;
+    elseif f_xr > 0 && f_xl < 0
+        xu = xr;
+    end
+    i = i + 1;
+end
+
+% Force out first error value
+tab(1,6) = NaN;
+
+x = linspace(0,5,1000);
+% Plotting
+plot(x,(tan(pi*x) - x - 6));
+axis([0 5 -10 10]);
+%% Question 2
+%% Part a
+% Define initial guess
+xi_1 = 0.48;
+% Initialize f(xi)
+f_x = 1;
+while abs(f_x) >= 1e-5
+    % Assign new value x_i+1 to xi
+    xi = xi_1;
+    % Find value of f(xi)
+    f_x = tan(pi*xi) - xi - 6; 
+    % Find value of df/dx at xi
+    df_dx = pi*sec(pi*xi)^2 - 1;
+    % Use N-R equation
+    xi_1 = xi - f_x/df_dx;
+end
+
+%% Part b
+% Define initial guesses
+x0 = 0.48;
+x1 = 0.54;
+% Initialize f(xi)
+f_x = 1;
+while abs(f_x) >= 1e-5
+    % Assign new value x_i+1 to xi
+    xi = xi_1;
+    % Find value of f(xi)
+    f_x = tan(pi*xi) - xi - 6; 
+    % Find value of f(xi-1)
+    f_x1 = pi*sec(pi*xi)^2 - 1;
+    % Use N-R equation
+    xi_1 = xi - f_x/;
+end
+
+
